246bfa2a38e06d724cdec132b0610ad4fa6daded
kadirumac <kadirumac49@gmail.com>
2020-03-23 20:48:10 +0100

    ok

---
+++ src/context.js
@@ -0,0 +1,70 @@
+import React, { Component ,createContext} from 'react';
+import { storeProducts,detailProduct} from './data';
+const ProductContext = createContext();
+//Provider
+//Consumer
+
+class ProductProvider extends Component {
+    state={
+        products:[],
+        detailProduct:detailProduct,
+        modalOpen:false,
+        modalProduct:detailProduct
+      
+    };
+    componentDidMount(){
+        this.setProducts();
+    }
+    setProducts = () =>{
+        let tempProducts = [];
+        storeProducts.forEach(item =>{
+            const singleItem ={...item};
+            tempProducts =[...tempProducts,singleItem]
+        })
+        this.setState(()=>{
+            return {products:tempProducts}
+        })
+    }
+
+    getItem = (id) =>{
+        const product = this.state.products.find(item=> item.id === id);
+        return product;
+    };
+    handleDetail = (id) =>{
+       const product = this.getItem(id);
+       this.setState(()=>{
+           return { detailProduct:product}
+       })
+    }
+
+    openModal =(id)=>{
+        const product = this.getItem(id);
+        this.setState(()=>{
+            return{modalProduct:product,modalOpen:true}
+        })
+    }
+
+ closeModal =()=>{
+     this.setState(()=>{
+         return{modalOpen:false}
+     })
+ }
+
+
+    render() {
+        return (
+            <ProductContext.Provider value={{
+                ...this.state,
+                handleDetail:this.handleDetail,        
+                openModal:this.openModal,
+                closeModal:this.closeModal
+            }}>
+                {this.props.children}    
+            </ProductContext.Provider>
+        );
+    }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export{ ProductProvider, ProductConsumer}
---
+++ src/data.js
@@ -0,0 +1,43 @@
+export const storeProducts = [
+    {
+      id: 1,
+      title: "Camera Shop",
+      img: "img/cam0.jpg",
+      type: "Front-end",
+      info:
+        "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+      
+      
+    },
+    {
+        id: 2,
+        title: "Chat-App",
+        img: "img/cam0.jpg",
+        type: "Full-Stack",
+        info:
+          "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+        
+        
+      },
+      {
+        id: 3,
+        title: "Hotel App",
+        img: "img/cam0.jpg",
+        type: "Front-end",
+        info:
+          "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+        
+        
+      }
+  ];
+  
+  export const detailProduct =   {
+    id: 1,
+    title: "Camera Shop",
+    img: "img/cam0.jpg",
+    type: "Front-end",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    
+    
+  };
