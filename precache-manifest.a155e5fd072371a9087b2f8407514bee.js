de1a85efbf93e01ac0d757dc701969f49c944c0c
kadirumac <kadirumac49@gmail.com>
2020-03-29 22:11:46 +0200

    Updates

---
+++ precache-manifest.a155e5fd072371a9087b2f8407514bee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b1dd94d4a5249ed3ec1c4ed63ef6a7",
+    "url": "/apply/index.html"
+  },
+  {
+    "revision": "857e371a55fc5b7dce93",
+    "url": "/apply/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "2c2416bf9a85cf32916b",
+    "url": "/apply/static/css/main.5b763687.chunk.css"
+  },
+  {
+    "revision": "857e371a55fc5b7dce93",
+    "url": "/apply/static/js/2.711078cc.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/apply/static/js/2.711078cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c2416bf9a85cf32916b",
+    "url": "/apply/static/js/main.95677c08.chunk.js"
+  },
+  {
+    "revision": "4059219b89778b27b282",
+    "url": "/apply/static/js/runtime-main.2a35fa94.js"
+  }
+]);
