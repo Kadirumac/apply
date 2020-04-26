830a4f673f1c9ed461f5828c1afb3d4c82a3e085
kadirumac <kadirumac49@gmail.com>
2020-04-26 23:28:09 +0200

    effect

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import './App.css';
 import {Switch,Route} from 'react-router-dom';
-
+import Intro from './pages/Intro'
 import ProjectList from './components/ProjectList';
 import Details from './components/Details';
 import Modal from './components/Modal';
@@ -11,6 +11,9 @@ import About from './components/About';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Physics from './demos/Physics';
+
 class App extends Component{
   
     render(){
@@ -18,16 +21,17 @@ class App extends Component{
     
           <React.Fragment>
           <Navbar/>
-          <Switch>
-        
-          <Route exact path="/" component={Home} ></Route>
-          <Route exact path={`${process.env.PUBLIC_URL}/project`} component={ProjectList}/>
-            {/* <Route exact path="/project"  component={ProjectList}></Route> */}
+       
+  
+          <Switch>          
+            <Route exact path="/" component={Home} ></Route>
+            <Route exact path="/project"  component={ProjectList}></Route> 
             <Route exact path="/details" component={Details}></Route>
             <Route exact path="/about" component={About} ></Route>
             <Route exact path="/contact" component={Contact} ></Route>
             <Route component={Default}></Route>
           </Switch>
+          <Physics />
           <Modal/>        
       </React.Fragment>
 
